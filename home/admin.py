dca4cf16f1f85191774bcac2017cb3e0c1003154
Matheus Lins <msl@cin.ufpe.br>
2015-01-17 16:58:17 -0300

    criando a primeia tabela de vacina

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Vacina)
--- home/models.py
+++ home/models.py
@@ -1,7 +1,25 @@
 from django.db import models
-from django.views.generic import View
 
-class home(View):
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('VACINOU !')
+class Vacina(models.Model):
+
+	vacina = models.CharField('Nome da Vacina', max_length = 100)
+	doenca = models.CharField('Doença', max_length = 100)
+	dose = models.CharField('Dose', max_length = 100)
+	dose_qtd = models.CharField('Dose_qtd', max_length = 100)
+	via_administracao = models.CharField('Via Administração', max_length = 100)
+	idade = models.CharField('Doença', max_length = 100)
+	image = models.ImageField(upload_to= 'home/images', verbose_name='Imagem', null = True, blank = True )
+	slug = models.SlugField('Atalho')
+	sobre = models.TextField('Sobre a Vacina', blank = True)
+	created_at = models.DateTimeField('Criado em', auto_now_add=True)
+	updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+
+	def __str__(self):
+		return self.vacina
+
+	class Meta:
+		verbose_name = 'Vacina'
+		verbose_name_plural = 'Vacinas'
+		ordering = ['vacina']
