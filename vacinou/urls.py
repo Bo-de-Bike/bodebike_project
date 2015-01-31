6cf90fe94dd776752cb36211d98187892dba6600
uchoavaz <joaouchoavaz@gmail.com>
2015-01-31 15:00:26 -0300

    django base2

--- vacinou/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-urlpatterns = patterns('',
-    url(r'^/', include('apps.core.urls', name = 'core')),
-    url(r'^admin/', include(admin.site.urls)),
-)
