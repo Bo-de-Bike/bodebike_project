eaa7520e4afa13edeb6933dad1ee4483a411fdc5
uchoavaz <joaouchoavaz@gmail.com>
2015-02-01 22:29:32 -0300

    Tabelas ok, corrigido bugs da alteracao, detalhes de vacina ok

--- vacinou/vacinas/views.py
+++ vacinou/vacinas/views.py
@@ -10,7 +10,7 @@ def detalhes(request,slug):
 	vacina = get_object_or_404(Vacina, slug=slug)
 	context['vacina'] = vacina
 
-	doencas = Doenca.objects.filter(id_vacina__name=vacina)
+	doencas = Doenca.objects.filter(id_vacina__nome=vacina)
 	context['doencas'] = doencas
 
 	return render(request, 'detalhesVacina.html', context)
